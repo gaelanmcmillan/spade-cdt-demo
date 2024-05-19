ba05de63aec71467ddc1eef188ff8e28b3d89037
Gaelan McMillan <gaelan.mcmillan@gmail.com>
2024-05-19 11:08:31 -0700

    add constraints to cdt

--- src/main.rs
+++ src/main.rs
@@ -24,11 +24,46 @@ async fn main() {
         mq::clear_background(mq::WHITE);
 
         if mq::is_mouse_button_pressed(mq::MouseButton::Left) {
-            let pt = mq::mouse_position_local();
-            let sc_pt = position_local_to_pixels(pt);
-            pts.push(pt);
-            cdt.insert(mq_vec2_to_spade_point2(sc_pt))
-                .expect("Could not add point");
+            // add single points when 'S' is pressed
+            if mq::is_key_down(mq::KeyCode::S) {
+                let pt = mq::mouse_position_local();
+                let sc_pt = position_local_to_pixels(pt);
+                pts.push(pt);
+                cdt.insert(mq_vec2_to_spade_point2(sc_pt))
+                    .expect("Could not add point");
+
+            // add boxes when 'B' is pressed
+            } else if mq::is_key_down(mq::KeyCode::B) {
+                let box_size_loc = 0.05;
+                let center = mq::mouse_position_local();
+                // clockwise from tl
+                let corners = vec![
+                    /* tl */ mq::Vec2::new(center.x - box_size_loc, center.y - box_size_loc),
+                    /* tr */ mq::Vec2::new(center.x + box_size_loc, center.y - box_size_loc),
+                    /* br */ mq::Vec2::new(center.x + box_size_loc, center.y + box_size_loc),
+                    /* bl */ mq::Vec2::new(center.x - box_size_loc, center.y + box_size_loc),
+                ];
+
+                for corner in corners.iter() {
+                    pts.push(*corner);
+                }
+
+                // insert each vertex into the triangulation, getting its handle.
+                let handles: Vec<_> = corners
+                    .iter()
+                    .map(|corner| {
+                        let local_pixels = position_local_to_pixels(*corner);
+                        let handle = cdt.insert(mq_vec2_to_spade_point2(local_pixels)).unwrap();
+                        handle
+                    })
+                    .collect();
+
+                for pair in handles.windows(2) {
+                    if cdt.can_add_constraint(pair[0], pair[1]) {
+                        cdt.add_constraint(pair[0], pair[1]);
+                    }
+                }
+            }
         }
 
         pts.iter().for_each(|pt| {
